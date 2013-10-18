9ba6b1fcceae67a21265ddc426c3244e860f890a
tryfinally <m@tryfinally.com>
2013-10-17 18:57:15 -0700

    added message sync + icons

--- src/main.c
+++ src/main.c
@@ -38,6 +38,23 @@ static struct WeatherData {
 } app;
 
 
+static void load_bitmap(uint32_t resource_id) {
+  // If that resource is already the current icon, we don't need to reload it
+  if (app.current_icon == resource_id) {
+    return;
+  }
+  
+  if (app.current_icon != 0) {
+    heap_bitmap_deinit(&app.icon_bitmap);
+  }
+  // Keep track of what the current icon is
+  app.current_icon = resource_id;
+  // Load the new icon
+  heap_bitmap_init(&app.icon_bitmap, resource_id);
+}
+
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+}
 
 
 void line_layer_update_callback(Layer *me, GContext* ctx) {
@@ -49,6 +66,21 @@ void line_layer_update_callback(Layer *me, GContext* ctx) {
 
 }
 
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
+
+  switch (key) {
+  case WEATHER_ICON_KEY:
+    load_bitmap(WEATHER_ICONS[new_tuple->value->uint8]);
+    bitmap_layer_set_bitmap(&app.icon_layer, &app.icon_bitmap.bmp);
+    break;
+	  
+  case WEATHER_TEMPERATURE_KEY:
+    text_layer_set_text(&app.temperature_layer, new_tuple->value->cstring);
+    break;
+  default:
+    return;
+  }
+}
 
 void handle_init(AppContextRef ctx) {
   resource_init_current_app(&WEATHER_APP_RESOURCES);
@@ -78,12 +110,31 @@ void handle_init(AppContextRef ctx) {
   app.line_layer.update_proc = &line_layer_update_callback;
   layer_add_child(&app.window.layer, &app.line_layer);
   
+	
+  GRect icon_rect = (GRect) {(GPoint) {4, 4}, (GSize) { 50, 50 }};
+  bitmap_layer_init(&app.icon_layer, icon_rect);
+  layer_add_child(&app.window.layer, &app.icon_layer.layer);
+
+  text_layer_init(&app.temperature_layer, GRect(0, 10, 144, 40));
+  text_layer_set_text_color(&app.temperature_layer, GColorWhite);
+  text_layer_set_background_color(&app.temperature_layer, GColorClear);
+  text_layer_set_font(&app.temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_alignment(&app.temperature_layer, GTextAlignmentRight);
+  layer_add_child(&app.window.layer, &app.temperature_layer.layer);
+
+	
   PblTm pt;
   get_time(&pt);
   void _handle_minute_tick(PblTm *tick_time);
   void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t);
   _handle_minute_tick(&pt);
-  // TODO: Update display here to avoid blank display on launch?
+	
+  Tuplet initial_values[] = {
+    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
+    TupletCString(WEATHER_TEMPERATURE_KEY, "99\u00B0C"),
+  };
+  app_sync_init(&app.sync, app.sync_buffer, sizeof(app.sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
+                sync_tuple_changed_callback, sync_error_callback, NULL);
 }
 
 
@@ -95,14 +146,15 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
 void _handle_minute_tick(PblTm *tick_time) {
 
   // Need to be static because they're used by the system later.
+  static char temp_text[] = "24 X";
   static char time_text[] = "00 00";
-  static char date_text[] = "Xxxxxxxxx  : 00";
+  static char date_text[] = "Xxxxxxxxx: 00";
 
   char *time_format;
 
 
   // TODO: Only update the date when it's changed.
-  string_format_time(date_text, sizeof(date_text), "%B  : %d", tick_time);
+  string_format_time(date_text, sizeof(date_text), "%b:%d", tick_time);
   text_layer_set_text(&app.text_date_layer, date_text);
 
 
@@ -119,15 +171,25 @@ void _handle_minute_tick(PblTm *tick_time) {
   if (!clock_is_24h_style() && (time_text[0] == '0')) {
     memmove(time_text, &time_text[1], sizeof(time_text) - 1);
   }
-
   text_layer_set_text(&app.text_time_layer, time_text);
+	
+  text_layer_set_text(&app.temperature_layer, temp_text);
 
+  load_bitmap(WEATHER_ICONS[2]);
+  bitmap_layer_set_bitmap(&app.icon_layer, &app.icon_bitmap.bmp);
 }
 
+static void handle_deinit(AppContextRef c) {
+  //app_sync_deinit(&app.sync);
+  if (app.current_icon != 0) {
+    heap_bitmap_deinit(&app.icon_bitmap);
+  }
+}
 
 void pbl_main(void *params) {
   PebbleAppHandlers handlers = {
     .init_handler = &handle_init,
+	.deinit_handler = &handle_deinit,
 
     .tick_info = {
       .tick_handler = &handle_minute_tick,
